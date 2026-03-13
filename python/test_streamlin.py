b3d646c4ec7483b90d4e0e5cda63ae396846f7bd
deshmukhamol100 <deshmukhamol100@gmail.com>
2026-03-13 13:51:59 +0000

    udpated 1

--- python/test_streamlin.py
+++ python/test_streamlin.py
@@ -4,14 +4,18 @@ import pandas as pd
 
 st.title("My First Streamlit App")  # This will display a title on the app
 st.write("this is simple app to demonstrate how to get started with Streamlit. You can use Streamlit to create interactive web applications with Python. Let's explore some of the features of Streamlit together.")# This will display some text on the app    
-st.sidebar.header("User Input features")  # This will create a header in the sidebar
 
-# This will create a slider in the sidebar for user input
-user_name = st.sidebar.slider("Select your name", "AMol deshmukh")
 
-#slider for age
-age = st.sidebar.slider("Select your age", 0, 100, 25)
+st.header("select a number")  # This will create a header in the app
 
-#selectbox for gender
-gender = st.sidebar.selectbox("Select your gender", ["Male", "Female", "Other"])    
+number = st.slider("Select a number", 0, 100, 25)  # This will create a slider for user input
 
+st.subheader("resuts")  # This will create a subheader in the app
+
+square = number * number
+
+# This will calculate the square of the selected number
+
+st.write("The square of the ", number, " selected number is:", square)  # This will display the result on the app
+
+#seletbox
