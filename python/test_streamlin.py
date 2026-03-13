7bdcae864b86558ab460a20e15af3c4833924200
deshmukhamol100 <deshmukhamol100@gmail.com>
2026-03-13 13:55:32 +0000

    update subtitile

--- python/test_streamlin.py
+++ python/test_streamlin.py
@@ -3,14 +3,14 @@ import numpy as np
 import pandas as pd
 
 st.title("My First Streamlit App")  # This will display a title on the app
-st.write("this is simple app to demonstrate how to get started with Streamlit. You can use Streamlit to create interactive web applications with Python. Let's explore some of the features of Streamlit together.")# This will display some text on the app    
+st.write("Welcome to my first Streamlit app! This is a simple calculator to find the square of a number.")# This will display some text on the app    
 
 
 st.header("select a number")  # This will create a header in the app
 
 number = st.slider("Select a number", 0, 100, 25)  # This will create a slider for user input
 
-st.subheader("resuts")  # This will create a subheader in the app
+st.subheader("Resuts")  # This will create a subheader in the app
 
 square = number * number
 
